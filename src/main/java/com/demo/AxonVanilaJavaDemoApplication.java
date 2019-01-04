44aff01895853263f81c819f7857c301dc2fd0c6
Ivan Dugalic <ivan.dugalic@axoniq.io>
2019-01-04 18:32:36 +0100

    Initial commit from Atomist

---
+++ src/main/java/com/demo/AxonVanilaJavaDemoApplication.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxonVanilaJavaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AxonVanilaJavaDemoApplication.class, args);
+    }
+}
