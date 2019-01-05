edfa54d7f5b3ec7f5b6721a3972b63600bfc8ece
Ivan Dugalic <ivan.dugalic@axoniq.io>
2019-01-05 18:40:59 +0100

    Minor fix

--- src/main/java/com/demo/AxonVanillaJavaDemoApplication.java
+++ src/main/java/com/demo/AxonVanillaJavaDemoApplication.java
@@ -56,7 +56,6 @@ public class AxonVanillaJavaDemoApplication {
         GiftCardRecord giftCardRecord = config.queryGateway().query(new FindGiftCardQry(randomId), GiftCardRecord.class).getNow(new GiftCardRecord("0", Integer.MIN_VALUE, Integer.MIN_VALUE));
         log.info("Result from `find card query`: " + giftCardRecord);
 
-        /* Shutdown */
         config.shutdown();
     }
 }
